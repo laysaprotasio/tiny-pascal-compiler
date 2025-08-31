9c9e88243d93ee351ac68e0001d9ca2c1796b88f
marianeme <marianedmsilva@gmail.com>
2025-08-31 15:02:34 -0300

    Implementa analisador semântico para if

--- src/parser/index.js
+++ src/parser/index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const TinyPascalLexer = require('../lexer/lexer');
 const TinyPascalParser = require('./parser');
+const {TinyPascalSemanticAnalyzer, SemanticError} = require('../semantics/semantic-analyzer');
 
 const filePath = path.resolve(__dirname, '../../example.tp');
 const exampleCode = fs.readFileSync(filePath, 'utf-8');
@@ -13,3 +14,12 @@ const parser = new TinyPascalParser(tokens);
 const result = parser.parseProgram();
 
 console.dir(result, { depth: null });
+
+const analyzer = new TinyPascalSemanticAnalyzer();
+const annotated = analyzer.analyzeProgram(result);
+
+console.dir(annotated, { depth: null });
+//console.log(analyzer.symbolTable);
+console.log("Tabela final:");
+console.log(JSON.stringify(analyzer.symbolTable, null, 2));
+
